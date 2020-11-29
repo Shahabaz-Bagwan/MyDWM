93fecf5898e2ef905e6345042871264f55f349ce
falconsb <>
2020-11-29 15:24:29 +0100

    added other touchpad options in autostart.sh

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -5,4 +5,8 @@ setxkbmap -option caps:swapescape
 xinput set-prop "SYN1B7D:01 06CB:2991 Touchpad" "libinput Tapping Enabled" 1
 
 xinput set-prop "SYN1B7D:01 06CB:2991 Touchpad" "libinput Natural Scrolling Enabled" 1
+
+xinput set-prop "SYNA2393:00 06CB:7A13 Touchpad" "libinput Tapping Enabled" 1
+xinput set-prop "SYNA2393:00 06CB:7A13 Touchpad" "libinput Natural Scrolling Enabled" 1
+
 dwmblocks
