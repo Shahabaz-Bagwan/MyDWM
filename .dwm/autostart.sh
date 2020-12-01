8ea598f6eb91a27c3ed557f04dc3fd2d5d51111f
falconsb <>
2020-12-01 11:53:06 +0100

    removed hide vacent patch

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -8,7 +8,7 @@ xinput set-prop "SYN1B7D:01 06CB:2991 Touchpad" "libinput Natural Scrolling Enab
 xinput set-prop "SYNA2393:00 06CB:7A13 Touchpad" "libinput Tapping Enabled" 1
 xinput set-prop "SYNA2393:00 06CB:7A13 Touchpad" "libinput Natural Scrolling Enabled" 1
 
-dwmblocks &
+dwmstatus 2>&1 >/dev/null &
 
 compton &
 
