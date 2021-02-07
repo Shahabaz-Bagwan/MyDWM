5681d352b38675b588cd53b9a3e0fcdbcb263144
Shahabaz <shahabaz@pop-os.localdomain>
2021-02-07 11:36:04 +0100

    update workspace numbers and added new laptop support

--- config.h
+++ config.h
@@ -24,7 +24,7 @@ static char *colors[][3] = {
        [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5" };
 
 static const Rule rules[] = {
   /* xprop(1):
