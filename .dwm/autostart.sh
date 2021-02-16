3da209de6d8a0a04147a6c40280a6a5087d346fa
falconsb <>
2021-02-16 10:10:36 +0100

    updated touchpad settings

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -8,9 +8,8 @@ setxkbmap -layout us -variant altgr-intl
 
 xinput set-prop "SYN1B7D:01 06CB:2991 Touchpad" "libinput Tapping Enabled" 1
 xinput set-prop "SYN1B7D:01 06CB:2991 Touchpad" "libinput Natural Scrolling Enabled" 1
-
-xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Tapping Enabled" 1
-xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Natural Scrolling Enabled" 1
+xinput set-prop "SYNA2393:00 06CB:7A13 Touchpad" "libinput Tapping Enabled" 1
+xinput set-prop "SYNA2393:00 06CB:7A13 Touchpad" "libinput Natural Scrolling Enabled" 1
 
 killall dwmblocks nm-applet blueman-applet pasystray
 dwmblocks 2>&1 >/dev/null &
