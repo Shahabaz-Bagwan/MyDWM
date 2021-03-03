44ba3d3366ae60a1517c472b860931c6a436c39f
falconsb <>
2021-03-03 12:56:52 +0100

    add Generic touchpad settings

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -6,8 +6,15 @@ export RANGER_LOAD_DEFAULT_RC=FALSE
 setxkbmap -option caps:swapescape
 setxkbmap -layout us -variant altgr-intl
 
+# Generic
+xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Tapping Enabled" 1
+xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Natural Scrolling Enabled" 1
+
+# Thinkpad
 xinput set-prop "SYN1B7D:01 06CB:2991 Touchpad" "libinput Tapping Enabled" 1
 xinput set-prop "SYN1B7D:01 06CB:2991 Touchpad" "libinput Natural Scrolling Enabled" 1
+
+# Precision
 xinput set-prop "SYNA2393:00 06CB:7A13 Touchpad" "libinput Tapping Enabled" 1
 xinput set-prop "SYNA2393:00 06CB:7A13 Touchpad" "libinput Natural Scrolling Enabled" 1
 
