129e840385fac083930606e857d5532547daf763
Shahabaz Bagwan <cool.shahabaz@gmail.com>
2021-08-09 10:47:57 +0200

    update autostart.sh

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -19,7 +19,7 @@ xinput set-prop "SYN1B7D:01 06CB:2991 Touchpad" "libinput Natural Scrolling Enab
 xinput set-prop "SYNA2393:00 06CB:7A13 Touchpad" "libinput Tapping Enabled" 1
 xinput set-prop "SYNA2393:00 06CB:7A13 Touchpad" "libinput Natural Scrolling Enabled" 1
 
-killall dwmblocks nm-applet blueman-applet pasystray
+killall dwmblocks nm-applet blueman-applet
 dwmblocks 2>&1 >/dev/null &
 
 compton &
